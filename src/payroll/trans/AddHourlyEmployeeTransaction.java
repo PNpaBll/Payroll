3b3073b34afcec0783dd7e41d6f7d7d5568d8f25
PNpaBll <changli19968@126.com>
2021-04-14 23:08:19 +0800

    为Transaction接口的实现类AddHourlyEmployeeTransaction添加未实现的方法

--- src/payroll/trans/AddHourlyEmployeeTransaction.java
+++ src/payroll/trans/AddHourlyEmployeeTransaction.java
@@ -16,4 +16,10 @@ public class AddHourlyEmployeeTransaction implements Transaction {
 		this.hourlyRate = hourlyRate;
 	}
 
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
