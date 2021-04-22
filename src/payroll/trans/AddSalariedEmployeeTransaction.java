8238712c29c8120411fc4c5bded341966ca669d6
PNpaBll <changli19968@126.com>
2021-04-22 22:58:06 +0800

    address赋给新字段

--- src/payroll/trans/AddSalariedEmployeeTransaction.java
+++ src/payroll/trans/AddSalariedEmployeeTransaction.java
@@ -6,11 +6,13 @@ public class AddSalariedEmployeeTransaction implements Transaction {
 
 	private int empId;
 	private String name;
+	private String address;
 
 	public AddSalariedEmployeeTransaction(int empId, String name, String address, double salary) {
 		this.empId = empId;
 		// TODO Auto-generated constructor stub
 		this.name = name;
+		this.address = address;
 	}
 
 	@Override
