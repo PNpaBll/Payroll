17d11d41eb22dd28b14de946bfe86d46d4d4c697
PNpaBll <changli19968@126.com>
2021-04-22 23:04:54 +0800

    salary赋给新字段

--- src/payroll/classification/SalariedClassification.java
+++ src/payroll/classification/SalariedClassification.java
@@ -4,7 +4,10 @@ import payroll.PaymentClassification;
 
 public class SalariedClassification extends PaymentClassification {
 
+	private double salary;
+
 	public SalariedClassification(double salary) {
+		this.salary = salary;
 		// TODO Auto-generated constructor stub
 	}
 
