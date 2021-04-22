88bd44f01ca2ff408031ca0992bb999a95c4300f
PNpaBll <changli19968@126.com>
2021-04-22 23:23:46 +0800

    创建新函数

--- src/payroll/trans/AddEmployeeTransaction.java
+++ src/payroll/trans/AddEmployeeTransaction.java
@@ -10,11 +10,20 @@ import payroll.method.HoldMethod;
 public abstract class AddEmployeeTransaction implements Transaction{
 
 	protected int empId;
+	protected String name;
+	protected String address;
+	public AddEmployeeTransaction(int empId, String name, String address) {
+		super();
+		this.empId = empId;
+		this.name = name;
+		this.address = address;
+	}
+
+
 
 	protected abstract PaymentClassification getPaymentClassification();
 
-	protected String name;
-	protected String address;
+	
 
 	public AddEmployeeTransaction() {
 		super();
