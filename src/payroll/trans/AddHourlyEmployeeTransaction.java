16a870bfb16c8155dc5d8bfb0a6eb83b182a26a2
PNpaBll <changli19968@126.com>
2021-04-22 23:10:37 +0800

    在AddHourlyEmployeeTransaction抽取方法

--- src/payroll/trans/AddHourlyEmployeeTransaction.java
+++ src/payroll/trans/AddHourlyEmployeeTransaction.java
@@ -1,7 +1,39 @@
 package payroll.trans;
 
+import payroll.Employee;
+import payroll.PayrollDatabase;
 import payroll.Transaction;
+import payroll.classification.HourlyClassification;
+import payroll.method.HoldMethod;
 
 public class AddHourlyEmployeeTransaction implements Transaction {
+
+	private int empId;
+	private String name;
+	private String address;
+	private double hourlyRate;
+
+	public AddHourlyEmployeeTransaction(int empId, String name, String address, double hourlyRate) {
+		this.empId = empId;
+		// TODO Auto-generated constructor stub
+		this.name = name;
+		this.address = address;
+		this.hourlyRate = hourlyRate;
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		Employee employee=new Employee(empId,name,address);
+		employee.setPaymentClassification(getPaymentClassification());
+		employee.setPaymentMethod(new HoldMethod());
+		PayrollDatabase.save(employee);
+//		employee.setPaymentClassification(new HourlyClassificaion(hourlyRate));
+	}
+
+	protected HourlyClassification getPaymentClassification() {
+		return new HourlyClassification(hourlyRate);
+	}
+
 
 }
