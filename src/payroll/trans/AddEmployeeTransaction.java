8797186f184d2d35159ff93a11f9fd82b5780243
PNpaBll <changli19968@126.com>
2021-04-22 23:22:27 +0800

    实现Transaction接口

---
+++ src/payroll/trans/AddEmployeeTransaction.java
@@ -0,0 +1,33 @@
+package payroll.trans;
+
+import payroll.Employee;
+import payroll.PaymentClassification;
+import payroll.PayrollDatabase;
+import payroll.Transaction;
+import payroll.classification.HourlyClassification;
+import payroll.method.HoldMethod;
+
+public abstract class AddEmployeeTransaction implements Transaction{
+
+	protected int empId;
+
+	protected abstract PaymentClassification getPaymentClassification();
+
+	protected String name;
+	protected String address;
+
+	public AddEmployeeTransaction() {
+		super();
+	}
+
+	@Override
+	public void execute() {
+			// TODO Auto-generated method stub
+			Employee employee=new Employee(empId,name,address);
+			employee.setPaymentClassification(getPaymentClassification());
+			employee.setPaymentMethod(new HoldMethod());
+			PayrollDatabase.save(employee);
+	//		employee.setPaymentClassification(new HourlyClassificaion(hourlyRate));
+		}
+
+}
