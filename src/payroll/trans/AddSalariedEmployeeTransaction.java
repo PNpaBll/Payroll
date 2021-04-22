7ecdf11c62db180f1aedbfc70583acb050eb27d8
PNpaBll <changli19968@126.com>
2021-04-22 22:58:36 +0800

    salary赋给新字段

--- src/payroll/trans/AddSalariedEmployeeTransaction.java
+++ src/payroll/trans/AddSalariedEmployeeTransaction.java
@@ -7,12 +7,14 @@ public class AddSalariedEmployeeTransaction implements Transaction {
 	private int empId;
 	private String name;
 	private String address;
+	private double salary;
 
 	public AddSalariedEmployeeTransaction(int empId, String name, String address, double salary) {
 		this.empId = empId;
 		// TODO Auto-generated constructor stub
 		this.name = name;
 		this.address = address;
+		this.salary = salary;
 	}
 
 	@Override
