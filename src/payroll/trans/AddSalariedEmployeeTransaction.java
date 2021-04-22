a90534c05907f6ab39619ffada1842afd412cf08
PNpaBll <changli19968@126.com>
2021-04-22 22:57:37 +0800

    name赋给新字段

--- src/payroll/trans/AddSalariedEmployeeTransaction.java
+++ src/payroll/trans/AddSalariedEmployeeTransaction.java
@@ -5,10 +5,12 @@ import payroll.Transaction;
 public class AddSalariedEmployeeTransaction implements Transaction {
 
 	private int empId;
+	private String name;
 
 	public AddSalariedEmployeeTransaction(int empId, String name, String address, double salary) {
 		this.empId = empId;
 		// TODO Auto-generated constructor stub
+		this.name = name;
 	}
 
 	@Override
