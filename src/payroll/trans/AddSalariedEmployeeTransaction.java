bc9b26de490d269733ba17417a4fabee8a57872f
PNpaBll <changli19968@126.com>
2021-04-22 22:57:03 +0800

    实现方法将empId赋给新字段

--- src/payroll/trans/AddSalariedEmployeeTransaction.java
+++ src/payroll/trans/AddSalariedEmployeeTransaction.java
@@ -4,7 +4,10 @@ import payroll.Transaction;
 
 public class AddSalariedEmployeeTransaction implements Transaction {
 
+	private int empId;
+
 	public AddSalariedEmployeeTransaction(int empId, String name, String address, double salary) {
+		this.empId = empId;
 		// TODO Auto-generated constructor stub
 	}
 
