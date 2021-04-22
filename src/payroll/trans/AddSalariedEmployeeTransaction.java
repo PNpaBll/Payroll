266852a9047081b7941ed39d77de3a5855374bbb
PNpaBll <changli19968@126.com>
2021-04-22 23:27:06 +0800

    AddSalariedEmployeeTransaction调用超类的构造函数

--- src/payroll/trans/AddSalariedEmployeeTransaction.java
+++ src/payroll/trans/AddSalariedEmployeeTransaction.java
@@ -1,33 +1,16 @@
 package payroll.trans;
 
-import payroll.Employee;
 import payroll.PaymentClassification;
-import payroll.PayrollDatabase;
 import payroll.Transaction;
 import payroll.classification.SalariedClassification;
-import payroll.method.HoldMethod;
 
-public class AddSalariedEmployeeTransaction implements Transaction {
+public class AddSalariedEmployeeTransaction extends AddEmployeeTransaction implements Transaction {
 
-	private int empId;
-	private String name;
-	private String address;
 	private double salary;
 
 	public AddSalariedEmployeeTransaction(int empId, String name, String address, double salary) {
-		this.empId = empId;
-		// TODO Auto-generated constructor stub
-		this.name = name;
-		this.address = address;
+		super(empId,name,address);
 		this.salary = salary;
-	}
-
-	@Override
-	public void execute() {
-		Employee employee=new Employee(empId,name,address);
-		employee.setPaymentClassification(getPaymentClassification());
-		employee.setPaymentMethod(new HoldMethod());
-		PayrollDatabase.save(employee);
 	}
 
 	protected PaymentClassification getPaymentClassification() {
