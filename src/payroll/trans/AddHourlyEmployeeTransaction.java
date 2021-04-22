f5a1866b87eacd215e61ba217d2af3a531dedde3
PNpaBll <changli19968@126.com>
2021-04-22 23:26:01 +0800

    AddHourlyEmployeeTransaction调用超类的构造函数

--- src/payroll/trans/AddHourlyEmployeeTransaction.java
+++ src/payroll/trans/AddHourlyEmployeeTransaction.java
@@ -1,37 +1,19 @@
 package payroll.trans;
 
-import payroll.Employee;
 import payroll.PaymentClassification;
-import payroll.PayrollDatabase;
 import payroll.Transaction;
 import payroll.classification.HourlyClassification;
-import payroll.method.HoldMethod;
 
-public class AddHourlyEmployeeTransaction implements Transaction {
+public class AddHourlyEmployeeTransaction extends AddEmployeeTransaction implements Transaction {
 
-	private int empId;
-	private String name;
-	private String address;
 	private double hourlyRate;
 
 	public AddHourlyEmployeeTransaction(int empId, String name, String address, double hourlyRate) {
-		this.empId = empId;
-		// TODO Auto-generated constructor stub
-		this.name = name;
-		this.address = address;
+		super(empId,name,address);
 		this.hourlyRate = hourlyRate;
 	}
 
 	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
-		Employee employee=new Employee(empId,name,address);
-		employee.setPaymentClassification(getPaymentClassification());
-		employee.setPaymentMethod(new HoldMethod());
-		PayrollDatabase.save(employee);
-//		employee.setPaymentClassification(new HourlyClassificaion(hourlyRate));
-	}
-
 	protected PaymentClassification getPaymentClassification() {
 		return new HourlyClassification(hourlyRate);
 	}
