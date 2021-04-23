5d09d5735791bc980ce1a4c5f1ee1d7771d32558
PNpaBll <changli19968@126.com>
2021-04-23 15:13:50 +0800

    添加新的测试

--- src/payroll/test/AddEmployeeTest.java
+++ src/payroll/test/AddEmployeeTest.java
@@ -11,8 +11,10 @@ import payroll.PaymentMethod;
 import payroll.PayrollDatabase;
 import payroll.Transaction;
 import payroll.classification.HourlyClassification;
+import payroll.classification.SalariedClassification;
 import payroll.method.HoldMethod;
 import payroll.trans.AddHourlyEmployeeTransaction;
+import payroll.trans.AddSalariedEmployeeTransaction;
 
 public class AddEmployeeTest {
 
@@ -59,4 +61,26 @@ public class AddEmployeeTest {
 		assertTrue(pn instanceof HoldMethod);//支付方式
 	}
 
+	@Test
+	public void testAddCommissionedEmployeeTransaction() {
+		int empId=1001;
+		String name="Bill";
+		String address="Home";
+		double salary=741.0;
+		double commissionRate=0.02;
+		Transaction t=new AddCommissionedEmployeeTransaction(empId,name,address,hourlyRate);
+		t.execute();
+		//验证执行结果
+		Employee e=PayrollDatabase.getEmployee(empId);//根据雇员编号读取雇员记录
+		assertNotNull(e);//雇员存在
+		assertEquals(empId,((Employee) e).getEmpId());
+		assertEquals(name,e.getName());
+		assertEquals(address,e.getAddress());
+		PaymentClassification pc=e.getPaymentClassification();
+		assertTrue(pc instanceof CommissionedClassification);//中点工
+		CommissionedClassification sc=(CommissionedClassification)pc;
+		assertEquals(salary,sc.getSalary(),0.01);//小时工资正确
+	
+		assertEquals(commissionRate,sc.getCommissionRate(),0.0001);
+	}
 }
