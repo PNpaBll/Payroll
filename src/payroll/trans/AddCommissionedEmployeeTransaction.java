93ede9bf647c486e7c9187887d421f3f747f7798
PNpaBll <changli19968@126.com>
2021-04-23 15:44:21 +0800

    赋值给字段

--- src/payroll/trans/AddCommissionedEmployeeTransaction.java
+++ src/payroll/trans/AddCommissionedEmployeeTransaction.java
@@ -1,12 +1,15 @@
 package payroll.trans;
 
 import payroll.PaymentClassification;
-
+import payroll.classification.*;
 public class AddCommissionedEmployeeTransaction extends AddEmployeeTransaction {
-
+	private double salary;
+	private double commissionRate;
 	public AddCommissionedEmployeeTransaction(int empId, String name, String address, double salary,
 			double commissionRate) {
 		super(empId,name,address);
+		this.salary=salary;
+		this.commissionRate=commissionRate;
 	}
 
 	@Override
